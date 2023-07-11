677906b4ba87d24ad964284055355af01c6e4508
BrianEtemesi <etemecci@gmail.com>
2023-07-11 21:02:01 +0000

    annotate arguments using Union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,10 @@
 """
 defines a type-annotated function `sum_mixed_list`
 """
+from typing import Union
 
 
-def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
     """
     takes list of integers and floats, sums the elements and
     returns sum as a float
