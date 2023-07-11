41d61fe0dcac6f0100fa5821153ed6e8e526c6ca
BrianEtemesi <etemecci@gmail.com>
2023-07-11 21:34:35 +0000

    import List from typing on task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,9 @@
 """
 defines a type-annotated function `sum_list`
 """
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     returns the sum of all elements in a list
     """
