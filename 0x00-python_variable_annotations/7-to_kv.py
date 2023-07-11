458be9e7e2b1fc2c59104d1756846936f831587d
BrianEtemesi <etemecci@gmail.com>
2023-07-11 21:26:05 +0000

    more complex types

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+defines a type-annotated function `to_kv`
+"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    returns string and square of v
+    """
+    return (k, v ** 2)
