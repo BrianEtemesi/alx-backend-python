510d9f6bf7fdecd19205b2457ef402e8eb77ab8e
BrianEtemesi <etemecci@gmail.com>
2023-07-11 20:11:55 +0000

    function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+defines a type annotated function `concat`
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates two strings
+    """
+    return str1 + str2
