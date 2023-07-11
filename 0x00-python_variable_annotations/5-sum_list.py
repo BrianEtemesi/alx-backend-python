9d250c305827ba80ec56a6e2c4bac246c3552e20
BrianEtemesi <etemecci@gmail.com>
2023-07-11 21:35:31 +0000

    pycode check

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ defines a type-annotated function `sum_list`
 """
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """
     returns the sum of all elements in a list
