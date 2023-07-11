5bbbeaab077fc8596c96ee977a1146c5329cf2a3
BrianEtemesi <etemecci@gmail.com>
2023-07-11 20:24:51 +0000

    get the floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+defines a type-annotated function `floor`
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of a floating point number
+    """
+    return math.floor(n)
