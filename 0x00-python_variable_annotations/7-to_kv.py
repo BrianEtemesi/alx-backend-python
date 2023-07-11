d9bb333d14f50a052ce93001a9f23ecffae7be77
BrianEtemesi <etemecci@gmail.com>
2023-07-11 21:26:44 +0000

    pycode check

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ defines a type-annotated function `to_kv`
 """
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     returns string and square of v
