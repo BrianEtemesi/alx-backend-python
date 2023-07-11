810eaef07b20d5dc63f6724f1957852745301319
BrianEtemesi <etemecci@gmail.com>
2023-07-11 20:06:35 +0000

    create new repo, add readme, submit add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+contains an annotated function `add`
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    adds two numbers and returns sum
+    """
+    return a + b
